fb8e4b2994ffd1613e6bcbb2e548c5d8e2a2c2c8
Eric Leow <ericleowyuquan@gmail.com>
2026-06-23 21:02:48 +0800

    (feat) Change line

--- src/main/java/com/sddevops/maven/eclipse3/App.java
+++ src/main/java/com/sddevops/maven/eclipse3/App.java
@@ -3,6 +3,6 @@ package com.sddevops.maven.eclipse3;
 public class App {
     public static void main(String[] args) {
         System.out.println("Eric test");
-        System.out.println("new line"); 
+        System.out.println("change line"); 
     }
 }
