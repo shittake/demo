9165de4f1ad02a6a7ce7ed03ba5b3a2e615ee371
Eric Leow <ericleowyuquan@gmail.com>
2026-06-25 19:03:38 +0800

    (feat) Test branch1

--- src/main/java/com/sddevops/maven/eclipse3/App.java
+++ src/main/java/com/sddevops/maven/eclipse3/App.java
@@ -3,6 +3,7 @@ package com.sddevops.maven.eclipse3;
 public class App {
     public static void main(String[] args) {
         System.out.println("Eric test");
-        System.out.println("change line"); 
+        System.out.println("change line branch"); 
     }
 }
+ 	
