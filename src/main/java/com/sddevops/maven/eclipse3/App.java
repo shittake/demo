8a4eed8f369eb150e478b5c6cda87a98a90314ad
Eric Leow <ericleowyuquan@gmail.com>
2026-06-25 21:12:12 +0800

    feat(student): Add password attribute to Student class and constructor

--- src/main/java/com/sddevops/maven/eclipse3/App.java
+++ src/main/java/com/sddevops/maven/eclipse3/App.java
@@ -9,8 +9,10 @@ public class App {
     
     class Student {
     	String name;
-    	public Student(String name) {
+    	String password;
+    	public Student(String name, String password) {
     		this.name = name;
+    		this.password = password;
     	}
     }
 }
